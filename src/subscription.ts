56e8995463566cdcac5cbca1cfbcf8779c357b68
dholms <dtholmgren@gmail.com>
2023-05-11 14:33:20 -0500

    comment out auth check & log firehose output

--- src/subscription.ts
+++ src/subscription.ts
@@ -8,6 +8,14 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
+
+    // This logs the text of every post off the firehose.
+    // Just for fun :)
+    // Delete before actually using
+    for (const post of ops.posts.creates) {
+      console.log(post.record.text)
+    }
+
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
