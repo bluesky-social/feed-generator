7ae30b335b8707a0a3772f3d397945a115f4abae
Edison Lee <edisonlee@edisonlee55.com>
2023-06-08 04:48:52 +0800

    fix: handle firehose subscription error reconnect (Close #44)

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,8 @@ const run = async () => {
       'wss://bsky.social',
     publisherDid:
       maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? 'did:example:alice',
+    subscriptionReconnectDelay:
+      maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
     hostname,
     serviceDid,
   })
