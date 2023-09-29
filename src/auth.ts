64162fa13674cdf23a241f362527ea7ec38d5e89
bryan newbold <bnewbold@robocracy.org>
2023-09-28 17:09:15 -0700

    fix basic build errors from deps bump

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyJwt, AuthRequiredError } from '@atproto/xrpc-server'
-import { DidResolver } from '@atproto/did-resolver'
+import { DidResolver } from '@atproto/identity'
 
 export const validateAuth = async (
   req: express.Request,
