7ff67ecb58aa04741b833e504d5b8532a890e7ee
Afternoon curry <afternooncurry@gmail.com>
2024-04-27 18:18:18 +0900

    externalize access URL for registering a feed

--- scripts/controlFeedGen.ts
+++ scripts/controlFeedGen.ts
@@ -49,7 +49,7 @@ const run = async () => {
     process.env.FEEDGEN_SERVICE_DID ?? `did:web:${process.env.FEEDGEN_HOSTNAME}`
 
   // only update this if in a test environment
-  const agent = new AtpAgent({ service: 'https://bsky.social' })
+  const agent = new AtpAgent({ service: process.env.FEEDGEN_REGISTRATION_URL ?? 'https://bsky.social'})
   await agent.login({ identifier: handle, password })
 
   let avatarRef: BlobRef | undefined
