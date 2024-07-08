3e3f846a9cb897006037c5241c3dd8fa279341c7
Daniel Holmgren <dtholmgren@gmail.com>
2024-07-08 12:59:26 -0500

    simplify db schema + pagination (#94)

--- src/algos/whats-alf.ts
+++ src/algos/whats-alf.ts
@@ -1,4 +1,3 @@
-import { InvalidRequestError } from '@atproto/xrpc-server'
 import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
 
@@ -14,15 +13,8 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
     .limit(params.limit)
 
   if (params.cursor) {
-    const [indexedAt, cid] = params.cursor.split('::')
-    if (!indexedAt || !cid) {
-      throw new InvalidRequestError('malformed cursor')
-    }
-    const timeStr = new Date(parseInt(indexedAt, 10)).toISOString()
-    builder = builder
-      .where('post.indexedAt', '<', timeStr)
-      .orWhere((qb) => qb.where('post.indexedAt', '=', timeStr))
-      .where('post.cid', '<', cid)
+    const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
+    builder = builder.where('post.indexedAt', '<', timeStr)
   }
   const res = await builder.execute()
 
@@ -33,7 +25,7 @@ export const handler = async (ctx: AppContext, params: QueryParams) => {
   let cursor: string | undefined
   const last = res.at(-1)
   if (last) {
-    cursor = `${new Date(last.indexedAt).getTime()}::${last.cid}`
+    cursor = new Date(last.indexedAt).getTime().toString(10)
   }
 
   return {
