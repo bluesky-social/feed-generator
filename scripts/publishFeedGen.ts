6500b6b5402e50d0999fa3ceda585087f21c2fbe
Hailey <hailey@blueskyweb.xyz>
2024-10-31 09:23:53 -0700

    Use inquirer for inputs over hardcoded values (#113)

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import inquirer from 'inquirer'
 import { AtpAgent, BlobRef } from '@atproto/api'
 import fs from 'fs/promises'
 import { ids } from '../src/lexicon/lexicons'
@@ -6,44 +7,64 @@ import { ids } from '../src/lexicon/lexicons'
 const run = async () => {
   dotenv.config()
 
-  // YOUR bluesky handle
-  // Ex: user.bsky.social
-  const handle = ''
-
-  // YOUR bluesky password, or preferably an App Password (found in your client settings)
-  // Ex: abcd-1234-efgh-5678
-  const password = ''
-
-  // A short name for the record that will show in urls
-  // Lowercase with no spaces.
-  // Ex: whats-hot
-  const recordName = ''
-
-  // A display name for your feed
-  // Ex: What's Hot
-  const displayName = ''
-
-  // (Optional) A description of your feed
-  // Ex: Top trending content from the whole network
-  const description = ''
-
-  // (Optional) The path to an image to be used as your feed's avatar
-  // Ex: ~/path/to/avatar.jpeg
-  const avatar: string = ''
-
-  // -------------------------------------
-  // NO NEED TO TOUCH ANYTHING BELOW HERE
-  // -------------------------------------
-
   if (!process.env.FEEDGEN_SERVICE_DID && !process.env.FEEDGEN_HOSTNAME) {
     throw new Error('Please provide a hostname in the .env file')
   }
+
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'handle',
+        message: 'Enter your Bluesky handle:',
+        required: true,
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Enter your Bluesky password (preferably an App Password):',
+      },
+      {
+        type: 'input',
+        name: 'service',
+        message: 'Optionally, enter a custom PDS service to sign in with:',
+        default: 'https://bsky.social',
+        required: false,
+      },
+      {
+        type: 'input',
+        name: 'recordName',
+        message: 'Enter a short name or the record. This will be shown in the feed\'s URL:',
+        required: true,
+      },
+      {
+        type: 'input',
+        name: 'displayName',
+        message: 'Enter a display name for your feed:',
+        required: true,
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Optionally, enter a brief description of your feed:',
+        required: false,
+      },
+      {
+        type: 'input',
+        name: 'avatar',
+        message: 'Optionally, enter a local path to an avatar that will be used for the feed:',
+        required: false,
+      },
+    ])
+
+  const { handle, password, recordName, displayName, description, avatar, service } = answers
+
   const feedGenDid =
     process.env.FEEDGEN_SERVICE_DID ?? `did:web:${process.env.FEEDGEN_HOSTNAME}`
 
   // only update this if in a test environment
-  const agent = new AtpAgent({ service: 'https://bsky.social' })
-  await agent.login({ identifier: handle, password })
+  const agent = new AtpAgent({ service: service ? service : 'https://bsky.social' })
+  await agent.login({ identifier: handle, password})
 
   let avatarRef: BlobRef | undefined
   if (avatar) {
