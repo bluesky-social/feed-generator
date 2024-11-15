a3d3e4aba2dd2996ffa3fb55f0fd55833fa6b02f
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2024-11-15 13:22:10 +0000

    changing algo name to beyhive

--- src/algos/beyhive.ts
+++ src/algos/beyhive.ts
@@ -2,7 +2,7 @@ import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import { AppContext } from '../config'
 
 // max 15 chars
-export const shortname = 'whats-alf'
+export const shortname = 'beyhive'
 
 export const handler = async (ctx: AppContext, params: QueryParams) => {
   let builder = ctx.db
--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as beyhive from './beyhive'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [beyhive.shortname]: beyhive.handler,
 }
 
 export default algos
