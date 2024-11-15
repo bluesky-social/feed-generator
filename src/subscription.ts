6d65b4e71dedb093e7b94426b5ca323e110a6e43
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2024-11-15 13:41:55 +0000

    removing posts with replies

--- src/subscription.ts
+++ src/subscription.ts
@@ -24,6 +24,9 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
+        if (create?.record?.hasOwnProperty('reply')) {
+          if (create.record.reply?.root !== null) return false
+        }
         // only alf-related posts
         const re =
           /^(?!.*(#beyboons|#haghive|haghive|hasbeyn)).*\b(beyonce|beyhive|beyoncé|yonce|yoncé|#beyonce)\b.*$/imu
