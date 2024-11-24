51436178169b2202216af9c6dd7ec4cb2134c16b
9TeenWolves <marshall.alexander19@yahoo.com>
2024-11-23 20:09:47 -0800

    Update well-known.ts

--- src/well-known.ts
+++ src/well-known.ts
@@ -4,21 +4,25 @@ import { AppContext } from './config'
 const makeRouter = (ctx: AppContext) => {
   const router = express.Router()
 
+  // Handle the .well-known/did.json route
   router.get('/.well-known/did.json', (_req, res) => {
-  res.json({
-    '@context': ['https://www.w3.org/ns/did/v1'],
-    id: ctx.cfg.serviceDid,
-    service: [
-      {
-        id: '#bsky_fg',
-        type: 'BskyFeedGenerator',
-        serviceEndpoint: `https://${ctx.cfg.hostname}`,
-      },
-    ],
+    if (!ctx.cfg.serviceDid.endsWith(ctx.cfg.hostname)) {
+      return res.sendStatus(404)
+    }
+    res.json({
+      '@context': ['https://www.w3.org/ns/did/v1'],
+      id: ctx.cfg.serviceDid,
+      service: [
+        {
+          id: '#bsky_fg',
+          type: 'BskyFeedGenerator',
+          serviceEndpoint: `https://${ctx.cfg.hostname}`,
+        },
+      ],
+    })
   })
-})
-
 
   return router
 }
+
 export default makeRouter
