68f1f58f234c507f80b2af1bec103d918653f689
Yochai Gal <yochai@discourse.rpgcauldron.com>
2024-11-28 22:54:34 -0500

    added osr algo to index.ts in algos folder

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,15 +1,9 @@
-import { AppContext } from '../config'
-import {
-  QueryParams,
-  OutputSchema as AlgoOutput,
-} from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import * as whatsAlf from './whats-alf';
+import * as osr from './osr';
 
-import * as osr from './osr'
-
-type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
-
-const algos: Record<string, AlgoHandler> = {
+const algos = {
+  [whatsAlf.shortname]: whatsAlf.handler,
   [osr.shortname]: osr.handler,
-}
+};
 
-export default algos
+export default algos;
