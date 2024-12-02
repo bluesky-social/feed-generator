2df4b31b6457aebeb663500110a7af1c0a1fe314
Michael Huntington <2466745+MikeHuntington@users.noreply.github.com>
2024-12-01 19:04:05 -0500

    testing new workflow with trigger

--- src/trigger/example.ts
+++ src/trigger/example.ts
@@ -1,16 +1,16 @@
-import { logger, task, wait } from '@trigger.dev/sdk/v3'
+import { logger, task, wait } from "@trigger.dev/sdk/v3";
 
-export const getFeedTask = task({
-  id: 'get-feed',
+export const helloWorldTask = task({
+  id: "hello-world",
   // Set an optional maxDuration to prevent tasks from running indefinitely
   maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
   run: async (payload: any, { ctx }) => {
-    logger.log('Hello, world!', { payload, ctx })
+    logger.log("Hello, world!", { payload, ctx });
 
-    await wait.for({ seconds: 5 })
+    await wait.for({ seconds: 5 });
 
     return {
-      message: 'Hello, world!',
+      message: "Hello, world!",
     }
   },
-})
+});
