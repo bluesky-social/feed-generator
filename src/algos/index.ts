8e82dd9aefa4e850acef06568c0129484a9f5243
CAVX <corey@blakeborough.me>
2024-12-11 16:08:38 -0700

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,14 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as survivor from './survivor'
+import * as survivordelay from './survivordelay'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [survivor.shortname]: survivor.handler,
+  [survivordelay.shortname]: survivordelay.handler,
 }
 
 export default algos
