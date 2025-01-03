69b8cc384e0300252d06d41afac474a3539560a1
Conlin Durbin <conlin.durbin@whatnot.com>
2025-01-03 16:53:10 -0500

    Remove mysql code

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
-import { Kysely, Migrator, MysqlDialect, PostgresDialect } from 'kysely'
-import { createPool } from 'mysql2'
+import { Kysely, Migrator, PostgresDialect } from 'kysely'
 import { DatabaseSchema } from './schema'
 import { migrationProvider } from './migrations'
 import { Pool } from 'pg'
@@ -10,26 +9,17 @@ const envPath = path.resolve(__dirname, '../../.env.local')
 dotenv.config({ path: envPath })
 
 export const createDb = async (): Promise<Database> => {
-  if (process.env.PG_DATABASE) {
-    const pg_dialect = new PostgresDialect({
-      pool: new Pool({
-        database: process.env.PG_DATABASE,
-        user: process.env.PG_USER,
-        password: process.env.PG_PASS,
-        host: process.env.PG_HOST,
-      }),
-    })
-    return new Kysely<DatabaseSchema>({
-      dialect: pg_dialect,
-    })
-  } else {
-    const dialect = new MysqlDialect({
-      pool: createPool(process.env.DATABASE_URL ?? ''),
-    })
-    return new Kysely<DatabaseSchema>({
-      dialect,
-    })
-  }
+  const pg_dialect = new PostgresDialect({
+    pool: new Pool({
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      host: process.env.PG_HOST,
+    }),
+  })
+  return new Kysely<DatabaseSchema>({
+    dialect: pg_dialect,
+  })
 }
 
 export const migrateToLatest = async (db: Database) => {
