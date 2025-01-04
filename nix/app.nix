56003486b334c410c2ddd1c1e83486568839e7e5
Conlin Durbin <conlin.durbin@whatnot.com>
2025-01-04 13:29:24 -0500

    Fix app nix creation

--- nix/app.nix
+++ nix/app.nix
@@ -1,26 +1,36 @@
 {
   name ? "",
-  yarn2nix-moretea,
   stdenv,
   inputs,
   nodejs-slim_22,
+  callPackage,
+  python3,
 }:
 let
   inherit (inputs) nix-filter;
+  project =
+    callPackage ../yarn-project.nix
+      {
+
+        nodejs = nodejs-slim_22;
+      }
+      {
+
+        src = nix-filter.lib {
+          root = ../.;
+          exclude = [
+            ../dist
+            ../node_modules
+          ];
+        };
+
+      };
 in
-yarn2nix-moretea.mkYarnPackage {
+project.overrideAttrs (oldAttrs: {
   inherit name;
-  src = nix-filter.lib {
-    root = ../.;
-    exclude = [
-      ../dist
-      ../node_modules
-    ];
-  };
   dontFixup = true;
-  doDist = false;
-  yarnLock = ../yarn.lock;
-  packageJSON = ../package.json;
+  buildInputs = oldAttrs.buildInputs ++ [ python3 ];
+
   buildPhase = ''
     # yarn tries to create a .yarn file in $HOME. There's probably a
     # better way to fix this but setting HOME to TMPDIR works for now.
@@ -30,8 +40,8 @@ yarn2nix-moretea.mkYarnPackage {
   installPhase = ''
     mkdir -p $out/bin
     cp -r node_modules $out/bin/node_modules
-    mv deps/feed-generator/dist $out/bin
-    mv deps/feed-generator/package.json $out/bin
+    mv dist $out/bin
+    mv package.json $out/bin
     cat <<ENTRYPOINT > $out/bin/entrypoint
     #!${stdenv.shell}
     ${nodejs-slim_22}/bin/node $out/bin/dist/index.js
@@ -39,4 +49,4 @@ yarn2nix-moretea.mkYarnPackage {
     chmod +x $out/bin/entrypoint
   '';
   meta.mainProgram = "entrypoint";
-}
+})
