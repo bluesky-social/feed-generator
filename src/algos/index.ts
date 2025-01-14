fdb69273fde75c79d2a9c782e32f51474699e8c2
Levhita Arias <levhita@gmail.com>
2025-01-14 16:04:59 -0600

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,14 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as devs from './planeta-devs'
+import * as linux from './planeta-linux'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [devs.shortname]: devs.handler,
+  [linux.shortname]: linux.handler,
 }
-
+console.log(algos)
 export default algos
