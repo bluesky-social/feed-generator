be8da4fd2a09c6624c61e9d260f3d6e6f7405923
Karz <karan_shukla@live.com>
2025-09-13 11:54:33 -0400

    revert

--- src/methods/feed-generation.ts
+++ src/methods/feed-generation.ts
@@ -10,7 +10,7 @@ export default function (server: Server, ctx: AppContext) {
     const feedUri = new AtUri(params.feed)
     const algo = algos[feedUri.rkey]
     if (
-      feedUri.hostname !== ctx.cfg.serviceDid ||
+      feedUri.hostname !== ctx.cfg.publisherDid ||
       feedUri.collection !== 'app.bsky.feed.generator' ||
       !algo
     ) {
