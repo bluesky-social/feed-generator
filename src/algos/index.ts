cdbdcee12e1ffa8856b902214d7f76bdb77fe281
Karz <karan_shukla@live.com>
2025-09-13 11:12:46 -0400

    fix short name

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,7 +3,7 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as navyFragen from './navy-fragen'
+import * as navyFragen from './navyfragen'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
